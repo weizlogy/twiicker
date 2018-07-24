bf2f33fe5220e7bc670cc724633609a1a1cbe22b
weizlogy <dreamfragment623+weizlogy@gmail.com>
2018-07-24 23:10:10 +0900

    PWA対応.

--- client/serviceworker.js
+++ client/serviceworker.js
@@ -5,3 +5,7 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   console.log('Service Worker activating.')
 })
+
+self.addEventListener('fetch', event => {
+  console.log('Service Worker fetching.')
+})
